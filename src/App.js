f812ae4cae91e832f6f3d63a6e593f3c773dd450
Ranjan.m <modranjan1999.com>
2022-06-16 15:18:59 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import SubNavBar from './components/SubNavBar'
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <SubNavBar />
+      <header className='App-header'></header>
+    </div>
+  )
+}
+
+export default App
